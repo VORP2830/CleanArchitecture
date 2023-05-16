49397771e329f8da22c60d29f991851eb16b4807
Victor Osório <victorporto_@hotmail.com>
2023-05-16 14:40:02 -0300

    iniciando a application

---
+++ CleanArchitecture.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "The price is required")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "The stock is required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        [DisplayName("Category")]
+        public Category Category { get; set; }
+    }
+}
---
+++ CleanArchitecture.Application/Services/CategoryService.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CleanArchitecture.Application.DTOs;
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMapper _mapper;
+        private ICategoryRepository _categoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task<CategoryDTO> GetById(int id)
+        {
+            var categoryEntity = await _categoryRepository.GetByIdAsync(id);
+            return _mapper.Map<CategoryDTO>(categoryEntity);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categoriesEntity = await _categoryRepository.GetCategoriesAsync();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+        }
+
+        public async Task Add(CategoryDTO categoryDTO)
+        {
+            var categoriesEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.CreateAsync(categoriesEntity);
+        }
+
+        public async Task Remove(int id)
+        {
+            var categoriesEntity = _categoryRepository.GetByIdAsync(id).Result;
+            await _categoryRepository.RemoveAsync(categoriesEntity);
+
+        }
+
+        public async Task Update(CategoryDTO categoryDTO)
+        {
+            var categoriesEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.UpdateAsync(categoriesEntity);
+        }
+    }
+}
