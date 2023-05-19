8f685858a469005c23a077c9beba8e0a3e04a41e
Victor Osório <victorporto_@hotmail.com>
2023-05-19 13:02:31 -0300

    Iniciando autenticação

--- CleanArchitecture.Infra.IoC/DependencyInjection.cs
+++ CleanArchitecture.Infra.IoC/DependencyInjection.cs
@@ -1,9 +1,12 @@
 using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.Mappings;
 using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Account;
 using CleanArchitecture.Domain.Interfaces;
 using CleanArchitecture.Infra.Data.Context;
+using CleanArchitecture.Infra.Data.Identity;
 using CleanArchitecture.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,10 +20,14 @@ namespace CleanArchitecture.Infra.IoC
             service.AddDbContext<ApplicationDbContext>(options => 
                 options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            service.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
             service.AddScoped<ICategoryRepository, CategoryRepository>();
             service.AddScoped<IProductRepository, ProductRepository>();
             service.AddScoped<IProductService, ProductService>();
             service.AddScoped<ICategoryService, CategoryService>();
+            service.AddScoped<IAuthenticate, AuthenticateService>();
+            
             service.AddAutoMapper(typeof(DomainToDTOMappingProfile));  
             var myHandlers = AppDomain.CurrentDomain.Load("CleanArchitecture.Application");
             service.AddMediatR(c => c.RegisterServicesFromAssembly(myHandlers));
